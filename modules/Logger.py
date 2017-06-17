33d6a8e73c1bf307f480dd979ff27c43974b15e6
Randy Gingeleski <rjg26247@gmail.com>
2017-06-16 20:12:31 -0400

    commit Logger

---
+++ modules/Logger.py
@@ -0,0 +1,21 @@
+from SystemParameters import LOGFILE_NAME
+
+class Logger(object):
+
+    @staticmethod
+    def writeout(msg):
+        """
+        Write out to file
+        """
+
+        with open('../' + LOGFILE_NAME, 'a') as file:
+            file.write('\n' + msg)
+
+    @staticmethod
+    def log(msg):
+        """
+        Default log method - prints and writes to file
+        """
+
+        print(msg)
+        Logger.writeout(msg)
