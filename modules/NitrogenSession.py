12eec288261d98899e9b8eb80e50f7611f401fbd
Randy Gingeleski <rjg26247@gmail.com>
2017-06-24 23:10:41 -0400

    improve NitrogenSession, clean up and add fault tolerance

--- modules/NitrogenSession.py
+++ modules/NitrogenSession.py
@@ -13,7 +13,7 @@ import time
 
 class NitrogenSession(object):
     """
-    Wraps the Nitrogen Sports API to abstract away nuances and logging statements
+    Wraps the Nitrogen Sports API to abstract away fault tolerance, logging and nuances...
     """
 
     def __init__(self):
@@ -21,26 +21,53 @@ class NitrogenSession(object):
         self.api = NitrogenApi()
 
         self.logged_in = False
-        self.last_login_time = -1
+        self.last_login_time = None
+
+        self.last_observed_balance = None
+        self.last_observed_inplay = None
 
     def login(self):
         """
         Log in to Nitrogen Sports
         """
 
-        self.api.login(NITROGEN_USER, NITROGEN_PASS)
-        self.logged_in = True
-        self.last_login_time = int(time.time())
-        time.sleep(1)
+        attempts = 0
+
+        while True:
+            status, balance, inplay = self.api.login(NITROGEN_USER, NITROGEN_PASS)
+            if status == 'SUCCCESS':
+                self.logged_in = True
+                self.last_login_time = int(time.time())
+                self.last_observed_balance = balance
+                self.last_observed_inplay = inplay
+                time.sleep(1)
+                return
+            elif attempts < 3:
+                attempts += 1
+                time.sleep(120)  # Try again in 2 minutes
+            else:
+                Logger.log('** Could not log in without error **')
+                raise RuntimeError('Could not log in without error')
 
     def logout(self):
         """
         Log out of Nitrogen Sports
         """
 
-        self.api.logout()
-        self.logged_in = False
-        time.sleep(1)
+        attempts = 0
+
+        while True:
+            status = self.api.logout()
+            if status == 'SUCCESS':
+                self.logged_in = False
+                time.sleep(1)
+                return
+            elif attempts < 3:
+                attempts += 1
+                time.sleep(120)  # Try again in 2 minutes
+            else:
+                Logger.log('** Could not log out without error **')
+                raise RuntimeError('Could not log out without error')
 
     def freshen_session(self):
         """
@@ -60,11 +87,10 @@ class NitrogenSession(object):
 
         self.freshen_session()
 
-        transaction_dump = self.api.get_transactions()
-        acct_balance = float(transaction_dump['transactionData']['balance'])
-        Logger.log('Observing account balance at ' + str(acct_balance) + ' BTC.')
+        balance = self.last_observed_balance
+        Logger.log('Recording account balance as ' + str(balance) + ' BTC')
 
-        return acct_balance
+        return balance
 
     def get_my_wagers(self):
         """
@@ -82,19 +108,19 @@ class NitrogenSession(object):
 
         self.freshen_session()
 
-        retries = 0
+        attempts = 0
 
         while True:
             time.sleep(2)
             data = self.api.find_upcoming_games()
             if data['errno'] == 0:
-                break
-            elif retries < 3:
-                retries += 1
+                return data
+            elif attempts < 3:
+                attempts += 1
+                time.sleep(120)  # Try again in 2 minutes
             else:
-                raise RuntimeError('Could not get upcoming games without error.')
-
-        return data
+                Logger.log('** Could not get upcoming games without error **')
+                raise RuntimeError('Could not get upcoming games without error')
 
     def find_league_games(self, league_key):
         """
@@ -102,23 +128,26 @@ class NitrogenSession(object):
 
         Args:
             league_key (str)
+
+        Returns:
+            (object)
         """
 
         self.freshen_session()
 
-        retries = 0
+        attempts = 0
 
         while True:
             time.sleep(2)
             data = self.api.find_games(league=league_key)
             if data['errno'] == 0:
-                break
-            elif retries < 3:
-                retries += 1
+                return data
+            elif attempts < 3:
+                attempts += 1
+                time.sleep(120)  # Try again in 2 minutes
             else:
+                Logger.log('** Could not get league games without error **')
                 raise RuntimeError('Could not get league games without error.')
-
-        return data
 
     def add_and_confirm_bet(self, event_id, period_id, bet_type, amount_to_bet):
         """
@@ -132,51 +161,60 @@ class NitrogenSession(object):
 
         if 'data' in add_bet_response:
             bet_id = add_bet_response['data'][0]['bet'][0]['bet_id']
-            Logger.log('Success, bet ID is ' + str(bet_id) + '.')
+            Logger.log('Bet ID is ' + str(bet_id))
             time.sleep(1)
-
-            # adjust risk to appropriate amount
-            Logger.log('Adjusting risk to ' + str(amount_to_bet) + ' BTC...')
             self.adjust_risk(bet_id, amount_to_bet)
             time.sleep(1)
-
-            Logger.log('Placing betslip...')
             self.place_betslip()
             time.sleep(1)
-
-            Logger.log('Confirming betslip...')
             self.confirm_betslip()
             time.sleep(1)
-
-            Logger.log('Bet in progress.')
         else:
-            Logger.log('** Something went wrong adding bet. **')
-            raise RuntimeError('Something went wrong adding bet.')
+            Logger.log('** Something went wrong adding bet **')
+            raise RuntimeError('Something went wrong adding bet')
+
+        Logger.log('Bet in progress.')
 
     def add_bet(self, event_id, period_id, bet_type):
         """
-        add_bet
+        Add bet
+
+        Returns:
+            (object)
         """
 
+        Logger.log('Adding bet... event ID ' + str(event_id) + ', period ID ' + str(period_id))
         return self.api.add_bet(event_id, period_id, bet_type)
 
     def adjust_risk(self, bet_id, amount_to_bet):
         """
-        adjust_risk
+        Adjust risk
+
+        Returns:
+            (object)
         """
 
+        Logger.log('Adjusting bet ID ' + str(bet_id) + ' risk to ' + str(amount_to_bet) + ' BTC')
         return self.api.adjust_risk(bet_id, str(amount_to_bet))
 
     def place_betslip(self):
         """
-        place_betslip
+        Place betslip
+
+        Returns:
+            (object)
         """
 
+        Logger.log('Placing betslip...')
         return self.api.place_betslip()
 
     def confirm_betslip(self):
         """
-        confirm_betslip
+        Confirm betslip
+        
+        Returns:
+            (object)
         """
 
+        Logger.log('Confirming betslip...')
         return self.api.confirm_betslip()
